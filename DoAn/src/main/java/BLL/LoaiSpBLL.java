a6e0716031b10dd377e24d9d2c261cb979060138
Cong Tran <ttcong2301@gmail.com>
2020-05-05 19:41:35 +0700

    3-Tier Architecture

--- DoAn/src/main/java/BLL/LoaiSpBLL.java
+++ DoAn/src/main/java/BLL/LoaiSpBLL.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BUS;
+package BLL;
 
 /**
  *
  * @author Cong
  */
-public class LoaiSpBUS {
+public class LoaiSpBLL {
   
 }
